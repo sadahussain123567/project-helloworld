294eb6c66a86a6a8170fdc81471fd01d7dfc3515
saddam hussain <159890907+sadahussain123567@users.noreply.github.com>
2024-03-11 05:26:33 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,6 @@
+//first thing is to create a folder in computer 
+//then open it on vscode
+//click  on new file 
+//create new file on vscode name it(example.ts)
+//write this command
 console.log("hello world")
