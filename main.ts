0cf41ffa6841649490eac7441ca182b4627f3a15
saddam hussain <159890907+sadahussain123567@users.noreply.github.com>
2024-03-11 05:27:30 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,3 +4,6 @@
 //create new file on vscode name it(example.ts)
 //write this command
 console.log("hello world")
+//then create a js file and compile it
+//for the result in cmd 
+//write node (file name).js
