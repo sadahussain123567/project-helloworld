e5221df053ad3739662b7a5c9c5ee1ae9f632c11
saddam hussain <159890907+sadahussain123567@users.noreply.github.com>
2024-03-11 05:27:53 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
-//first thing is to create a folder in computer 
-//then open it on vscode
-//click  on new file 
-//create new file on vscode name it(example.ts)
-//write this command
 console.log("hello world");
-//then create a js file and compile it
-//for the result in cmd 
-//write node (file name).js
